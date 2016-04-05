661b80205c5e2931840ce7ec14121812acd68a58
Patrick Perrone <patrickperrone@users.noreply.github.com>
2016-04-04 21:11:17 -0400

    Added a custom FormHandler similar to Sitecore's.
    * Added a custom FormHandler
    
    * Generated new distributive package.
    
    * Removed old distributive package.

---
+++ src/Feature/LayoutSimulator/code/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Sitecore.Feature.LayoutSimulator.HtmlHelpers;
+using Sitecore.Mvc.Helpers;
+
+namespace Sitecore.Feature.LayoutSimulator.Extensions
+{
+    public static class HtmlHelper
+    {
+        public static CustomSitecoreHelper LayoutSimulator(this System.Web.Mvc.HtmlHelper htmlHelper)
+        {
+            // get the helper from current thread
+            var threadData = ThreadHelper.GetThreadData<CustomSitecoreHelper>();
+            if (threadData != null) return threadData;
+
+            // create new helper if needed
+            var helper = new CustomSitecoreHelper(htmlHelper);
+            ThreadHelper.SetThreadData(helper);
+            return helper;
+        }
+    }
+}
