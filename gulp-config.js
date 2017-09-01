ebc4cda952130137afe36977d0d7dae1536d8e2a
SP_FarmAdmin <pperrone123@gmail.com>
2017-09-01 14:21:03 -0400

    Updated dev instance settings

--- gulp-config.js
+++ gulp-config.js
@@ -1,9 +1,9 @@
 module.exports = function () {
-    var config = {
-    websiteRoot: "C:\\inetpub\\SIM\\habitat81u1\\Website",
-    sitecoreLibraries: "C:\\inetpub\\SIM\\habitat81u1\\Website\\bin",
-    solutionName: "Layout Simulator",
-    buildConfiguration: "Debug"
-    }
-    return config;
+	var config = {
+		websiteRoot: "C:\\inetpub\\SIM\\habitat82u4\\Website",
+		sitecoreLibraries: "C:\\inetpub\\SIM\\habitat82u4\\Website\\bin",
+		solutionName: "Layout Simulator",
+		buildConfiguration: "Debug"
+	}
+	return config;
 }
