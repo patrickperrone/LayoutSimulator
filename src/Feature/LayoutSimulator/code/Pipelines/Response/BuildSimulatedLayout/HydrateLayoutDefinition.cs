dd7879ebdd2237325da5edb5414149bb7da1f83c
SP_FarmAdmin <pperrone123@gmail.com>
2017-09-29 10:25:30 -0400

    Made log statements clearer.

--- src/Feature/LayoutSimulator/code/Pipelines/Response/BuildSimulatedLayout/HydrateLayoutDefinition.cs
+++ src/Feature/LayoutSimulator/code/Pipelines/Response/BuildSimulatedLayout/HydrateLayoutDefinition.cs
@@ -7,43 +7,44 @@ using Sitecore.Feature.LayoutSimulator.Extensions;
 
 namespace Sitecore.Feature.LayoutSimulator.Pipelines.Response.BuildSimulatedLayout
 {
-    public class HydrateLayoutDefinition : BuildSimulatedLayoutProcessor
-    {
-        public override void Process(BuildSimulatedLayoutArgs args)
-        {
-            SimulatedLayout simulatedLayout = GetFromMemory();
+	public class HydrateLayoutDefinition : BuildSimulatedLayoutProcessor
+	{
+		public override void Process(BuildSimulatedLayoutArgs args)
+		{
+			SimulatedLayout simulatedLayout = GetFromMemory();
 
-            if (simulatedLayout == null)
-            {
-                args.AbortPipeline();
-                return;
-            }
+			if (simulatedLayout == null)
+			{
+				Log.Warn("Simulated layout is null.".ToStringForLogging(), this);
+				args.AbortPipeline();
+				return;
+			}
 
-            try
-            {
-                args.LayoutToSimulate = XElement.Parse(simulatedLayout.LayoutDefinition);
-            }
-            catch (Exception exc)
-            {
-                Log.Error($"Failed to convert SimulatedLayout object to xml. {exc.Message}".ToStringForLogging(), this);
-                args.AbortPipeline();
-                return;
-            }
-        }
+			try
+			{
+				args.LayoutToSimulate = XElement.Parse(simulatedLayout.LayoutDefinition);
+			}
+			catch (Exception exc)
+			{
+				Log.Error($"Failed to convert SimulatedLayout object to xml. {exc.Message}".ToStringForLogging(), this);
+				args.AbortPipeline();
+				return;
+			}
+		}
 
-        private SimulatedLayout GetFromMemory()
-        {
-            var parameters = HttpUtility.ParseQueryString(HttpContext.Current.Request.QueryString.ToString());
-            CacheManager cm = new CacheManager();
-            string cacheKey = cm.GetCacheKey(parameters[Configuration.Settings.QueryStringKey]);
+		private SimulatedLayout GetFromMemory()
+		{
+			var parameters = HttpUtility.ParseQueryString(HttpContext.Current.Request.QueryString.ToString());
+			CacheManager cm = new CacheManager();
+			string cacheKey = cm.GetCacheKey(parameters[Configuration.Settings.QueryStringKey]);
 			SimulatedLayout cacheValue = CacheManager.cache.GetCacheValue(cacheKey) as SimulatedLayout;
 
 			if (cacheValue == null)
-            {
-                Log.Error($"Failed to find SimulatedLayout object in cache with cacheKey [{cacheKey}]".ToStringForLogging(), this);
-            }
+			{
+				Log.Error($"Failed to find SimulatedLayout object in cache with cacheKey [{cacheKey}]".ToStringForLogging(), this);
+			}
 
-            return cacheValue;
-        }
-    }
+			return cacheValue;
+		}
+	}
 }
--- src/Feature/LayoutSimulator/code/Pipelines/Response/BuildSimulatedLayout/ValidateData.cs
+++ src/Feature/LayoutSimulator/code/Pipelines/Response/BuildSimulatedLayout/ValidateData.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Web;
 using System.Collections.Specialized;
 using Sitecore.Diagnostics;
 using Sitecore.Mvc.Extensions;
@@ -7,49 +6,49 @@ using Sitecore.Feature.LayoutSimulator.Extensions;
 
 namespace Sitecore.Feature.LayoutSimulator.Pipelines.Response.BuildSimulatedLayout
 {
-    public class ValidateData : BuildSimulatedLayoutProcessor
-    {
-        public override void Process(BuildSimulatedLayoutArgs args)
-        {
-            Assert.ArgumentNotNull(args, "args");
+	public class ValidateData : BuildSimulatedLayoutProcessor
+	{
+		public override void Process(BuildSimulatedLayoutArgs args)
+		{
+			Assert.ArgumentNotNull(args, "args");
 
-            args.ApplySimulatedDefinition = false;
+			args.ApplySimulatedDefinition = false;
 
-            if (args.OriginalPageDefinition == null || args.QueryStringParameters == null)
-            {
-                args.AbortPipeline();
-                return;
-            }
+			if (args.OriginalPageDefinition == null || args.QueryStringParameters == null)
+			{
+				args.AbortPipeline();
+				return;
+			}
 
-            if (args.QueryStringParameters.Get(Configuration.Settings.QueryStringKey).IsEmptyOrNull())
-            {
-                args.AbortPipeline();
-                return;
-            }
+			if (args.QueryStringParameters.Get(Configuration.Settings.QueryStringKey).IsEmptyOrNull())
+			{
+				args.AbortPipeline();
+				return;
+			}
 
-            if (!ValidQueryStringParameter(args.QueryStringParameters))
-            {
-                args.AbortPipeline();
-                return;
-            }
-        }
+			if (!ValidQueryStringParameter(args.QueryStringParameters))
+			{
+				args.AbortPipeline();
+				return;
+			}
+		}
 
-        public bool ValidQueryStringParameter(NameValueCollection parameters)
-        {
-            foreach (var kvp in parameters.ToKeyValues())
-            {
-                if (kvp.Key.Equals(Configuration.Settings.QueryStringKey))
-                {
-                    Guid result = new Guid();
-                    if (!Guid.TryParse(kvp.Value.ToString(), out result))
-                    {
-                        Log.Warn($"Value of [{Configuration.Settings.QueryStringKey}] must be a Guid".ToStringForLogging(), this);
-                        return false;
-                    }
-                }
-            }
+		public bool ValidQueryStringParameter(NameValueCollection parameters)
+		{
+			foreach (var kvp in parameters.ToKeyValues())
+			{
+				if (kvp.Key.Equals(Configuration.Settings.QueryStringKey))
+				{
+					Guid result = new Guid();
+					if (!Guid.TryParse(kvp.Value.ToString(), out result))
+					{
+						Log.Warn($"Value of [{Configuration.Settings.QueryStringKey}] must be a Guid".ToStringForLogging(), this);
+						return false;
+					}
+				}
+			}
 
-            return true;
-        }
-    }
+			return true;
+		}
+	}
 }
